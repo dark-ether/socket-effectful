388d5429a6d43c1ac3c445729ee4e6fb113ed617
dark-ether <henrique.hsa2002@gmail.com>
2023-02-23 14:25:51 -0300

    first commit

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "adaptation of the socket library for the effecful ecosystem";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    haskell-flake.url = "github:srid/haskell-flake";
+    mission-control.url = "github:Platonic-Systems/mission-control";
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, treefmt-nix,mission-control, haskell-flake,... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+        treefmt-nix.flakeModule
+        mission-control.flakeModule
+        haskell-flake.flakeModule
+      ];
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+        haskellProjects.main = {
+          packages.socket-effectful.root = ./.;
+          devShell = {
+            enable = true;
+            # add shellHook when merged
+            tools = hp: { treefmt = config.treefmt.build.wrapper;};
+          };
+        };
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+        treefmt.config = {
+          programs = {
+            cabal-fmt.enable = true;
+            hlint.enable = true;
+          };
+          projectRootFile = "flake.nix";
+        };
+        devShells.default = config.devShells.main;
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
+}
---
+++ test/Effectful/Network/Socket/Test.hs
@@ -0,0 +1,45 @@
+module Effectful.Network.Socket.Test where
+import Effectful
+import Effectful.Network.Socket
+import Control.Monad(join)
+import Test.Tasty
+import Test.QuickCheck
+import Test.QuickCheck.Monadic
+import Test.Tasty.QuickCheck
+import Test.QuickCheck.Instances.ByteString()
+import Data.ByteString
+import Control.Monad.Catch
+
+connectsAndReceives:: ByteString -> SocketE :> es => Eff es Bool
+connectsAndReceives message = do
+  bracket 
+    (do
+      sock <- socket ::SocketE :> es=> Eff es (Socket Inet Stream TCP)
+      effSetSocketOption sock $ ReuseAddress True
+      return sock
+    )
+    close
+    (\s -> do 
+      bind s $ SocketAddressInet (inetAddressFromTuple (127,0,0,1)) 8080
+      listen s 5
+      bracket 
+        (do 
+          sock <- socket :: SocketE :> es => Eff es (Socket Inet Stream TCP)
+          effSetSocketOption sock $ ReuseAddress True
+          return sock
+        )
+        close 
+        (\client -> do 
+          connect client $ SocketAddressInet (inetAddressFromTuple (127,0,0,1)) 8080
+          (clientSV,_)<- accept s
+          _ <- send clientSV message mempty
+          rmess<-receive client 4096 mempty
+          pure $ rmess == message
+        )
+    )
+
+prop_car::Property
+prop_car = verbose . ioProperty . join . fmap (runEff . runSocket) . generate . monadic' $ do
+  bs <- pick arbitrary
+  prop <- run $ connectsAndReceives bs
+  assert prop
